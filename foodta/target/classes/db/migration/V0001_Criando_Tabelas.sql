a310bf961ac761ee6fd13e1a024eb7a7a768d242
Belasco <162700278+Belasco08@users.noreply.github.com>
2025-05-21 08:56:29 -0300

    Add files via upload

--- foodta/target/classes/db/migration/V0001_Criando_Tabelas.sql
+++ foodta/target/classes/db/migration/V0001_Criando_Tabelas.sql
@@ -35,7 +35,7 @@ Create table tb_grupo(
 
 Create table tb_grupo_permissao(
     grupo_id bigint not null,
-    permissao_id bigint not null,
+    permissao_id bigint not null
     primary key(grupo_id, permissao_id)
     )engine=InnoDB default charset=utf8;
 
